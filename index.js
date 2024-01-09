d9ad2f610f0ce63ed5723e142eea13b661e4dfbb
Yashvir Singh <yashveernathawat@gmail.com>
2024-01-09 19:25:43 +0530

    Admin Routes

--- index.js
+++ index.js
@@ -1,17 +1,110 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+
+const USER_SECRET = "user";
+const ADMIN_SECRET = "admin";
+app.use(express.json());
+
+//Middlewares
+const authenticateUser = (req,res,next)=>{
+    const authHeader = req.headers.authorization;
+    if(authHeader){
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token,USER_SECRET,(err,user)=>{
+            if(err){
+                return res.sendStatus(403);
+            }
+            console.log(user);
+            req.user = user;
+            next();
+        });
+    }
+    else{
+        res.sendStatus(401);
+    }
+}
+const authenticateAdmin = (req,res,next)=>{
+    const authHeader = req.headers.authorization;
+    if(authHeader){
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token,ADMIN_SECRET,(err,user)=>{
+            if(err){
+                return res.sendStatus(403);
+            }
+            console.log(user);
+            req.user = user;
+            next();
+        });
+    }
+    else{
+        res.sendStatus(401);
+    }
+}
+
+//Schema
+const userSchema = mongoose.Schema({
+    username: String,
+    password: String,
+    purchasedCourses: [{type: mongoose.Schema.Types.ObjectId,ref: 'Course' }]
+});
+
+const courseSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageLink: String,
+    published: Boolean
+});
+
+const adminSchema = mongoose.Schema({
+    username: String,
+    password: String,
+});
+
+const User = mongoose.model('User',userSchema);
+const Course = mongoose.model('Course',courseSchema);
+const Admin = mongoose.model('Admin',adminSchema);
+
+//Connect to database
+mongoose.connect('');
 
 // Admin
-app.post('/admin/signup',(req,res)=>{
-    res.send('Admin SignUp');
+app.post('/admin/signup',async (req,res)=>{
+    const {username,password} = req.body;
+    
+    const user = await Admin.findOne({username});
+    console.log(user);
+    if(!user){
+        const obj = {username,password};
+        const newAdmin = new Admin(obj);
+        await newAdmin.save();
+        const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
+        res.send({message: 'User created successfully!!',token});
+    }
+    else{
+        res.status(404).send({message: 'Admin already exist!'});
+    }
 });
 
-app.post('/admin/login',(req,res)=>{
-    res.send('Admin LogIn');
+app.post('/admin/login', async (req,res)=>{
+    const {username,password} = req.headers;
+    const user = await Admin.findOne({username,password});
+    if(user){
+        const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
+        res.send({message: 'LogIn successfully!!',token});
+    }
+    else{
+        res.status(404).send({message: 'Admin does not exist!'});
+    }
 });
 
-app.post('/admin/addCourse',(req,res)=>{
-    res.send('Add Course');
+app.post('/admin/addCourse',authenticateAdmin , async (req,res)=>{
+    const course = new Course(req.body);
+    await course.save();
+    res.json({ message: 'Course created successfully', courseId: course.id });
 });
 
 app.get('/admin/getCourse',(req,res)=>{
@@ -19,8 +112,21 @@ app.get('/admin/getCourse',(req,res)=>{
 });
 
 // User
+app.post('/user/signup',(req,res)=>{
 
+});
+app.post('/user/login',(req,res)=>{
 
+});
+app.post('/user/courses/:courseId',(req,res)=>{
+
+});
+app.get('/user/courses',(req,res)=>{
+
+});
+app.get('/user/purchasedCourses',(req,res)=>{
+
+});
 
 
 
